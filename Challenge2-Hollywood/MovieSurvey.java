c194e6eff7eb1c8fd92ad13f0b14ae9057dc02bc
kevinc3 <kevinc3@d6886180-81a5-43a1-86a0-6ff1f76a6444>
2016-09-14 00:25:01 +0000

    Movie Survey Finished

--- Challenge2-Hollywood/MovieSurvey.java
+++ Challenge2-Hollywood/MovieSurvey.java
@@ -2,11 +2,42 @@
  * A program to run a simple survey and report the results. See MovieSurvey.txt
  * for more information. TODO: add your netid to the line below
  * 
- * @author put-your-netid-here
+ * @author kevinc3
  */
 public class MovieSurvey {
 	public static void main(String[] arg) {
 		// TODO: Write your program here
+		
+		//INTRO LINES
+		System.out.println("Welcome. We're interested in how people are watching movies this year.");
+		System.out.println("Thanks for your time. - The WRITERS GUILD OF AMERICA.");
+		System.out.println("Please tell us about how you've watched movies in the last month.");
+		//ask for 3 quantities: number of movies watched at 1.cinema 2.dvd player 3.computer
+		int cinemaMovies;
+			System.out.println("How many movies have you seen at the cinema?");
+			cinemaMovies = TextIO.getlnInt();
+		int dvdPlayer;
+			System.out.println("How many movies have you seen using a DVD or VHS player?");
+			dvdPlayer = TextIO.getlnInt();
+		int computerMovie;
+				System.out.println("How many movies have you seen using a Computer?");
+				computerMovie = TextIO.getlnInt();
+		//print summary of input values
+		System.out.println("Summary: " + cinemaMovies + " Cinema movies, " + dvdPlayer + " DVD/VHS movies, " + computerMovie + " Computer movies");
+		//print out total movies watched in one month
+		int total = cinemaMovies + dvdPlayer + computerMovie;
+			System.out.println("Total: " + total + " movies");
+		//print out percent of movies watched at cinema--2decimal places
+		float percentCinema = (float)((cinemaMovies*100.00f)/total);
+			System.out.print("Fraction of movies seen at a cinema: ");
+			TextIO.putf("%.2f", percentCinema);
+			System.out.print("%\n");
+		//print out percent of movies watched outside of cinema--2decimal places
+		float percentNotCinema = (float)(((dvdPlayer + computerMovie)*100.00f)/total);
+			System.out.print("Fraction of movies seen outside of a cinema: ");
+			TextIO.putf("%.2f",  percentNotCinema);
+			System.out.print("%");
+		
 		// Hints :
 		// Formatted output
 		// http://math.hws.edu/javanotes/c2/s4.html#basics.4.4
