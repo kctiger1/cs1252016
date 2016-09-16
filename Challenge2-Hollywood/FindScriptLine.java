02f41145c9a6cb444d52b1debe8b7ca3aa509777
kevinc3 <kevinc3@d6886180-81a5-43a1-86a0-6ff1f76a6444>
2016-09-16 19:16:59 +0000

    NOT COMPLETE YET!! WILL CHANGE

--- Challenge2-Hollywood/FindScriptLine.java
+++ Challenge2-Hollywood/FindScriptLine.java
@@ -1,13 +1,52 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Text;
+
 /**
  * A program to search for specific lines and print their line number.
  * See FindScriptLine.txt for more details
  * TODO: add your netid to the line below
- * @author put-your-netid-here
+ * @author kevinc3
  */
 public class FindScriptLine {
 
 	public static void main(String[] args) {
+		
 // TODO: Implement the functionality described in FindScriptLine.txt
 // TODO: Test your code manually and using the automated unit tests in FindScriptLineTest		
+		boolean output = false;
+		String word = "";
+		
+		TextIO.putln("Please enter the word(s) to search for");
+		word = TextIO.getln();
+		
+
+		
+		TextIO.readFile("thematrix.txt");
+		
+		System.out.println("Searching for " + "'" + word + "'");
+		word = word.toLowerCase();
+		output = false;
+		int counter = 0;
+		while (false == TextIO.eof()){
+			
+			String line = TextIO.getln();
+			counter++;
+			
+			if (line.isEmpty())
+				output = false;
+			
+			if (line.indexOf(word) >= 0)
+				output = true;				
+			
+			if (output){
+				line = line.trim();
+				if (line.contains(word))
+					TextIO.putln(counter + " - " + line);					
+				
+					
+			}
+			
+		}
+		System.out.println("Done Searching for " + "'" + word + "'");
+	
 	}
 }
