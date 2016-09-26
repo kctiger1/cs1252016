ee21737a64cf79b8b0ec7763cf52b6bfa454663c
kevinc3 <kevinc3@d6886180-81a5-43a1-86a0-6ff1f76a6444>
2016-09-26 21:07:48 +0000

    Cipher Breaker done!!

--- Challenge3-TopSecret/CipherBreaker.java
+++ Challenge3-TopSecret/CipherBreaker.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 /**
  * See CipherBreaker.txt for instructions.
  * TODO: add your netid to the line below
  * 
- * @author put-your-netid-here
+ * @author kevinc3
  */
 public class CipherBreaker {
 
@@ -10,6 +12,71 @@ public class CipherBreaker {
 		TextIO.putln("Text?");
 		String line = TextIO.getln();
 		TextIO.putln(line);
-
+		line = line.toUpperCase();
+		
+		char[] cipher = line.toCharArray();
+		int s=0,f=-1;
+		Arrays.sort(cipher);
+		int space = 0, punc = 0, digit = 0;
+		
+		for (int i = 0; i< line.length(); i++){		//initial for
+			for (int j = 0; j<line.length();j++){	//frequency
+				if(cipher[i] == cipher[j])
+					s++;
+			}	//end frequency for
+			for (int k = 0; k<i;k++){	//check duplicates
+			if(cipher[i]==cipher[k])
+				f = 1;
+			}	//end duplicate for
+			
+			if(f==-1 && cipher[i]>=65 && cipher[i]<=90){
+			   System.out.println(cipher[i] +":" + s);
+			}
+			  
+			  if (cipher[i] == ' '){
+				 space +=1;
+			 }
+			  if (cipher[i] == '"' || cipher[i] == '\'' || cipher[i] == '.' || cipher[i] == '-' || cipher[i] == '!' || cipher[i] == ','){
+					 punc +=1;
+				 }
+			  if (cipher[i]>=48 && cipher[i]<=57){
+				  digit +=1;
+			  }
+			 
+			 
+			  s=0;
+			  f=-1;
+			
+			
+		}	//end initial for
+		if (digit != 0){
+			System.out.println("DIGITS" + ":" + digit);
+		}
+		if (space != 0)	{
+			System.out.println("SPACES" +  ":" + space);
+		}
+		if (punc != 0){
+			System.out.println("PUNCTUATION" +  ":" + punc);
+		}
+		
+			// Print the letter's frequency
+		  /*if(f==-1 && line.charAt(i) != ' ' && line.charAt(i) != '"' && line.charAt(i) != '.' && line.charAt(i) != '-' && line.charAt(i) != '!' && line.charAt(i) != ',' 
+				  && line.charAt(i) != '\'')
+		   System.out.println(line.charAt(i) +":" + s);
+		  
+		  if (line.charAt(i) == ' '){
+			 System.out.println("SPACES" +  ":" + s);
+		 }
+		  if (line.charAt(i) == '"' || line.charAt(i) == '\'' || line.charAt(i) == '.' || line.charAt(i) == '-' || line.charAt(i) == '!' || line.charAt(i) == ','){
+				 System.out.println("PUNCTUATION" +  ":" + s);
+			 }
+		  if (Character.isDigit(i)){
+			  System.out.println("DIGITS" + ":" + s);
+		  }
+		 
+		  s=0;
+		  f=-1;
+		 
+		}*/
 	}
 }
