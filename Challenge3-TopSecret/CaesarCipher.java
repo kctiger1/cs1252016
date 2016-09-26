0a6aecc2e9e115d352e3706e0bdc0db091cd5049
kevinc3 <kevinc3@d6886180-81a5-43a1-86a0-6ff1f76a6444>
2016-09-26 23:14:16 +0000

    fml

--- Challenge3-TopSecret/CaesarCipher.java
+++ Challenge3-TopSecret/CaesarCipher.java
@@ -7,13 +7,92 @@
  * Good Programming Hints: "DRY: Don't Repeat Yourself"
  * Try to make your program as short as possible.
  * TODO: add your netid to the line below
- * @author put-your-netid-here
+ * @author kevinc3
  */
 public class CaesarCipher {
 
 	public static void main(String[] strings) {
-		//TODO: Delete the following line and write your implementation here- 
-		throw new RuntimeException ("Et tu Brutus?");
+		
+		int offset = 0;
+		
+		while ((offset == 0 || offset > 25 || offset<-25) && offset != 999 && offset != -999){
+			TextIO.putln("Please enter the shift value (between -25..-1 and 1..25)");
+			offset = TextIO.getlnInt();
+			if ((offset == 0 || offset > 25 || offset<-25) && offset != 999 && offset != -999){
+				System.out.println(offset + " is not a valid shift value.");
+			}
+			
+		}
+		
+		if ((offset != 0 || offset < 25 || offset > -25) && offset != 999 && offset != -999){
+		System.out.println("Using shift value of " + offset);
+		}
+		
+		if (offset == 999 || offset == -999){
+			System.out.println("Using position shift");
+		}
+		
+		boolean loop = true;
+		String sourceText;
+		
+		while(loop){
+			TextIO.putln("Please enter the source text (empty line to quit)");
+			sourceText = TextIO.getlnString();
+			if (sourceText.equals("")){
+				System.out.print("Bye.");
+				loop = false;
+			}
+			else{
+				System.out.println("Source   :" + sourceText);
+				sourceText = sourceText.toUpperCase();
+				String encrypted = "";
+				for(int i = 0; i < sourceText.length(); i++)
+					        {
+					            int c = sourceText.charAt(i);
+					            if (Character.isLetter(c)){
+					            if (c != ' ' ){
+					            	if (c >= 32 && c<= 127&& offset != 999 && offset != -999){
+					            c = c + (offset % 26);
+					                 if (c > 'Z'){
+					                 c = c - 26;
+					                 }
+					                 if (c < 'A'){
+					                	 c = c + 26;
+					                 }
+					            	}
+					            	else if (offset == 999){
+					            		for(int j=0;j<sourceText.length();j++){
+					            		c = c + j;
+					            		}
+					            	}
+					            	else if(offset == -999){
+					            		for(int k=0;k<sourceText.length();k++){
+					            		c = c - k;
+					            		if (c > 'Z'){
+							                 c = c - 26;
+							                 }
+							                 if (c < 'A'){
+							                	 c = c + 26;
+							                 }
+					            		}
+					            	}	
+					            	}
+					            }
+					            
+					        encrypted += (char) c;   
+					        }
+				sourceText = encrypted;
+				System.out.println("Processed:" + sourceText);
+				
+				
+				if (sourceText.equals("")){
+				System.out.print("Bye.");
+				loop = false;
+				}
+			}
+		
+		}	//end of while																
+		
 	}
 
 }
