33e60153b1e11f19e5ecf68fe0d1ef010b9f9ec7
kevinc3 <kevinc3@d6886180-81a5-43a1-86a0-6ff1f76a6444>
2016-10-05 05:03:45 +0000

    Finished PlayListUtilities!

--- Challenge4-Photoscoop/PlayListUtil.java
+++ Challenge4-Photoscoop/PlayListUtil.java
@@ -1,39 +1,74 @@
 /**
  * Add you netid here..
- * @author angrave
+ * 
+ * @author kevinc3
  *
  */
 public class PlayListUtil {
 
 	/**
-	 * Debug ME! Use the unit tests to reverse engineer how this method should work.
-	 * Hint: Fix the formatting (shift-cmd-F) to help debug the following code
+	 * Debug ME! Use the unit tests to reverse engineer how this method should
+	 * work. Hint: Fix the formatting (shift-cmd-F) to help debug the following
+	 * code
+	 * 
 	 * @param list
 	 * @param maximum
 	 */
 	public static void list(String[] list, int maximum) {
 		int i;
-		for ( i = 0      ; i    <=    maximum; ); {      TextIO.putln(   ""  + i + ". " + list[i]);}
+		for (i = 0; (i<list.length && maximum ==-1) || i<maximum;i++)
+		{
+			TextIO.putln("" + (i+1) + ". " + list[i]);
+		}
 	}
 
 	/**
 	 * Appends or prepends the title
+	 * 
 	 * @param list
 	 * @param title
-	 * @param prepend if true, prepend the title otherwise append the title
-	 * @return a new list with the title prepended or appended to the original list
+	 * @param prepend
+	 *            if true, prepend the title otherwise append the title
+	 * @return a new list with the title prepended or appended to the original
+	 *         list
 	 */
 	public static String[] add(String[] list, String title, boolean prepend) {
-		return list;
+		int appendedLength = list.length + 1;
+		String[] appended = new String[appendedLength];
+		if (prepend){
+			appended[0] = title;
+			for (int i = 1; i < appendedLength;i++){
+				appended[i] = list[i-1];
+			}
+		}
+		else{
+			for (int j = 0; j < appendedLength-1; j++){
+				appended[j] = list[j];
+			}
+			appended[appendedLength - 1] = title;
+		}
+		return appended;
 	}
+
 	/**
 	 * Returns a new list with the element at index removed.
-	 * @param list the original list
-	 * @param index the array index to remove.
+	 * 
+	 * @param list
+	 *            the original list
+	 * @param index
+	 *            the array index to remove.
 	 * @return a new list with the String at position 'index', absent.
 	 */
 	public static String[] discard(String[] list, int index) {
-		return list;
+		int discardLength = list.length - 1;
+		String[] discard = new String[discardLength];
+		for(int i = 0; i < discardLength; i++){
+			discard[i] = list[i];
+		}
+		for (int i = index + 1; i < discardLength+1; i++){
+			discard[i-1] = list[i];
+		}
+		return discard;
 	}
 
 }
