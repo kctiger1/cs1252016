4fa7c2ea53102d324c1e0bf80993bc380836f43b
kevinc3 <kevinc3@d6886180-81a5-43a1-86a0-6ff1f76a6444>
2016-10-13 02:03:30 +0000

    3/8 done!

--- Challenge5-DataStructures/UsingPublicFieldsIsEasy.java
+++ Challenge5-DataStructures/UsingPublicFieldsIsEasy.java
@@ -9,10 +9,23 @@
  *   where a,b, and M are replaced with the numerical values of a, b and the multiplication of a and b.
  * Your code will create a SimplePublicTriple, initializes the three fields and return a reference to the SimplePublicTriple object.
  *
+ *@author kevinc3
  */
 public class UsingPublicFieldsIsEasy {
 	
 	public static SimplePublicTriple analyze(SimplePublicPair in) {
-		
+		SimplePublicTriple result = new SimplePublicTriple();
+		int M;
+		if (in.a < in.b){
+			result.x = in.a;
+			result.y = in.b;
+		}
+		else{
+			result.x = in.b;
+			result.y = in.a;
+		}
+		M = result.x * result.y;
+		result.description = in.a + "*" + in.b + "=" + M;
+		return result;
 	}
 }
