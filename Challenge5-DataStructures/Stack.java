96a6bad769e4b08e88e00863f71b22466a50998e
kevinc3 <kevinc3@d6886180-81a5-43a1-86a0-6ff1f76a6444>
2016-10-22 21:17:38 +0000

    Finished 8/8

--- Challenge5-DataStructures/Stack.java
+++ Challenge5-DataStructures/Stack.java
@@ -1,32 +1,54 @@
-
+//@author kevinc3
 public class Stack {
+	private String[] stack = new String[1];
+	private int size = 0;
 	/** Adds a value to the top of the stack.*/
 	public void push(String value){
-		throw new RuntimeException("Elevator stuck. Abort Retry Fail?");
+		size++;
+		String[] old = new String[size - 1];
+		for (int i = 0; i < size-1; i++){
+			old[i] = stack[i];
+		}
+		stack = new String[size];
+		stack[0] = value;
+		for (int i = 1; i<stack.length; i++){
+			stack[i] = old[i-1];
+		}
 	}
 	
 	/** Removes the topmost string. If the stack is empty, returns null. */
 	public String pop() {
-		throw new RuntimeException("Ones and Zeros. Mostly.");
+		if (size == 0) return null;
+		String last = stack[0];
+		size--;
+		String[] old = stack;
+		for (int i = 0; i < size; i++){
+			this.stack[i] = old[i+1];
+		}
+		return last;
 	}
 	
 	/** Returns the topmost string but does not remove it. If the stack is empty, returns null. */
 	public String peek() {
-		throw new RuntimeException("Don't peek. It's too scary");
+		return this.stack[0];
 	}
 	
 	/** Returns true iff the stack is empty */
 	public boolean isEmpty() {
-		throw new RuntimeException("Need more");
+		return size==0;
 	}
 
 	/** Returns the number of items in the stack. */
 	public int length() {
-		throw new RuntimeException("Must be relative");
+		return size;
 	}
 	
 	/** Returns a string representation of the stack. Each string is separated by a newline. Returns an empty string if the stack is empty. */
 	public String toString() {
-		throw new RuntimeException("Rope is thicker but String is quicker");
+		String result = "";
+		for (int i = this.size - 1; i >= 0; i--){
+			result = result + this.stack[i] + "\n";
+		}
+		return result;
 	}
 }
