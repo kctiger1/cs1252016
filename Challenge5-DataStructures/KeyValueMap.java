bb884555045f2c6dfc6f91675b43ba14b7fc389e
kevinc3 <kevinc3@d6886180-81a5-43a1-86a0-6ff1f76a6444>
2016-10-22 20:18:39 +0000

    Finished 6/8

--- Challenge5-DataStructures/KeyValueMap.java
+++ Challenge5-DataStructures/KeyValueMap.java
@@ -1,34 +1,73 @@
+//@author kevinc3
 import java.awt.Color;
 
 public class KeyValueMap { // aka Dictionary or Map
-
+	private String[] string1 = new String [0];
+	private int size = 0;
+	private Color[] color = new Color[0];
 	/**
 	 * Adds a key and value. If the key already exists, it replaces the original
 	 * entry.
 	 */
 	public void add(String key, Color value) {
-		throw new RuntimeException("Hi");
+		size++;
+		String[] old = string1;
+		string1 = new String[size];
+		string1[0] = key;
+		for (int i = 1; i < string1.length; i++){
+			string1[i] = old[i-1];
+		}
+		Color[] old2 = color;
+		color = new Color[size];
+		color[0] = value;
+		for (int i = 1; i< color.length; i++){
+			color[i] = old2[i-1];
+		}
 	}
 
 	/**
 	 * Returns particular Color object previously added to this map.
 	 */
 	public Color find(String key) {
-		throw new RuntimeException("Ho");
+		for (int i = 0; i< size; i++){
+			if(key.equals(string1[i])){
+				return color[i];
+			}
+		}
+		return null;
 	}
 
 	/**
 	 * Returns true if the key exists in this map.
 	 */
 	public boolean exists(String key) {
-		throw new RuntimeException("Hi");
+		for (int i = 0; i < size; i++) {
+			if (key.equals(string1[i])){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	/**
 	 * Removes the key (and the color) from this map.
 	 */
 	public void remove(String key) {
-		throw new RuntimeException("Ho");
+		int found = 0;
+		for (int i = 0; i < size; i++){
+			if (key.equals(string1[i])){
+				found = i;
+			}
+		}
+		size--;
+		String[] old = string1;
+		string1 = new String[size];
+		for (int i = 0; i < found; i++) {
+			string1[i] = old[i];
+		}
+		for (int i = found+1; i < size; i++) {
+			string1[i-1] = old[i];
+		} 	
 	}
 
 }
