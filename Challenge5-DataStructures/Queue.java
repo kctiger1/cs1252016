a04da599eb383c41a7d10ff7e117a619c4008064
kevinc3 <kevinc3@d6886180-81a5-43a1-86a0-6ff1f76a6444>
2016-10-22 21:02:07 +0000

    Finished 7/8

--- Challenge5-DataStructures/Queue.java
+++ Challenge5-DataStructures/Queue.java
@@ -1,27 +1,51 @@
-
+//@author kevinc3
 public class Queue {
+	private double[] queue = new double[0];
+	private int size = 0;
 	/** Adds the value to the front of the queue.
 	 * Note the queue automatically resizes as more items are added. */
 	public void add(double value) {
-		throw new RuntimeException("Don't step on the cracks");
+		size ++;
+		double[] old = queue;
+		queue = new double[size];
+		queue[0] = value;
+		for (int i = 1; i < queue.length; i++){
+			queue[i] = old[i-1];
+		}
 	}
 	/** Removes the value from the end of the queue. If the queue is empty, returns 0 */
 	public double remove() {
-		throw new RuntimeException("Grilled Cheese");
+		if (size == 0){
+			return 0.0;
+		}
+		size --;
+		double[] old = queue;
+		for (int i = 0; i < size; i++){
+			queue[i] = old[i];
+		}
+		return queue[size];
 	}
 	
 	/** Returns the number of items in the queue. */
 	public int length() {
-		throw new RuntimeException("I am not a number; I am free man.");		
+		return size;
 	}
 	
 	/** Returns true iff the queue is empty */
 	public boolean isEmpty() {
-		throw new RuntimeException("The butler did it");
+		if(size == 0){
+			return true;
+		}
+		return false;
 	}
 	
 	/** Returns a comma separated string representation of the queue. */
 	public String toString() {
-		throw new RuntimeException("Daisy daisy daisy");
+		String result = "";
+		result = result + this.queue[this.size-1];
+		for (int i = this.size - 2; i >= 0; i--){
+			result = result + "," + this.queue[i];
+		}
+		return result;
 	}
 }
